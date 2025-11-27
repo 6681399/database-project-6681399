a4460222b2255c0ee814075214e2c824860712c8
Nattapas Nunthameteesuk <Nattapas.nun@student.mahidol.edu>
2025-11-27 17:28:08 +0700

    added shop owner and login page for barber

--- add_barber.sql
+++ add_barber.sql
@@ -1,14 +1,30 @@
-create function add_barber(p_company_id integer, p_first_name character varying, p_last_name character varying, p_commission numeric) returns integer
+-- auto-generated definition
+create function add_barber(p_company_id integer, p_first_name character varying, p_last_name character varying, p_email character varying, p_phone_number character varying, p_password character varying) returns integer
     language plpgsql
 as
 $$
 DECLARE v_id INT;
 BEGIN
-    INSERT INTO Barber_Profile (Company_ID, First_Name, Sur_Name, Commission_Rate)
-    VALUES (p_company_id, p_first_name, p_last_name, p_commission)
+    INSERT INTO Barber_Profile (
+        Company_ID,
+        First_Name,
+        Sur_Name,
+        Email,
+        Mobile_Tel,
+        Password_Hash
+    )
+    VALUES (
+        p_company_id,
+        p_first_name,
+        p_last_name,
+        p_email,
+        p_phone_number,
+        crypt(p_password, gen_salt('bf'))
+    )
     RETURNING Barber_ID INTO v_id;
+
     RETURN v_id;
 END; $$;
 
-alter function add_barber(integer, varchar, varchar, numeric) owner to root;
+alter function add_barber(integer, varchar, varchar, varchar, varchar, varchar) owner to root;
 
